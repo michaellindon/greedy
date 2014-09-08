e6f12ef0fa0527fae623c7acd368e90e04d8600c
Michael Lindon <michael.s.lindon@gmail.com>
2014-09-08 14:01:45 -0400

    Running selection for t-priors

--- t.m
+++ t.m
@@ -26,10 +26,8 @@ y=x*b+sqrt(1/phi)*randn(n,1);
 %Subtract Mean (because integrating out intercept over uniform prior)
 y=y-mean(y);
 
-%ols for reference
-Bols=linsolve(x'*x,x'*y);
-
 %Model Parameters
+alpha=1;
 priorprob=0.01*ones(p,1); %Predictor inclusion probability
 logpriorprob=log(priorprob);
 priorodds=priorprob./(1-priorprob); %Predictor inclusion odds
@@ -40,7 +38,8 @@ xx=x'*x;
 
 %Calculate (log)Density of Null Model (all up to proportionality constant)
 gamma=zeros(p,1);
-logdensity=ones(p,1)*sum(gamma.*logpriorodds);
+B=zeros(p,1);
+logdensity=(-1)*ones(p,1)*exp(100);
 logdensityold=logdensity;
 
 %Begin Greedy Algorithm
@@ -48,37 +47,43 @@ sort_improv=ones(p,1);
 fprintf('Order of egressors Added\n');
 while sort_improv(1)>0 %While still improving the log posterior density
    
-%%%%%%%%%%%%%%%%%%%%%%%%Begin Expectation Step%%%%%%%%%%%%%%%%%%%%%%%%
+    %Construct sub-matrices
+   inc_indices=find(gamma); %Included Indices
+   Lamg=Lam(inc_indices,inc_indices); 
+   xxg=xx(inc_indices,inc_indices);
+   xg=x(:,inc_indices);
+   Bg=B(inc_indices,1);
+    
+%%%%%%%%%%%%%%%%%%%%%%%%Begin Expectation Step%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    lam=(alpha+gamma)./(alpha+phi*gamma.*B.*B);
    Lam=diag(lam);
    Lamg=Lam(inc_indices,inc_indices); 
-%%%%%%%%%%%%%%%%%%%%%%%%End Expectation Step%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%End Expectation Step%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
    
 %%%%%%%%%%%%%%%%%%%%%%%%Begin Maximization for phi%%%%%%%%%%%%%%%%%%%%%%%%
-    b=(double)0.5*dot(yo-xog*Bg,yo-xog*Bg)+0.5*dot(Bg,Lamg*Bg);
-	a=(double)0.5*(no+sum(gamma)-3);
+    b=0.5*dot(y-xg*Bg,y-xg*Bg)+0.5*dot(Bg,Lamg*Bg);
+	a=0.5*(n+sum(gamma)-3);
 	phi=a/b;
-%%%%%%%%%%%%%%%%%%%%%%%%End Maximization for phi%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%End Maximization for phi%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-%%%%%%%%%%%%%%%%%%%%%%%%Begin Maximization of (Beta,Gamma)%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%Begin Maximization of (Beta,Gamma)%%%%%%%%%%%%%%%%%
 %Greedy Part    
 for i=1:p
    gamma_prop=gamma; %Gamma is binary vector for inclusion/exclusion
    gamma_prop(i)=1; %Proposed gamma
    inc_indices=find(gamma_prop); %Included Indices
-   
-   
-
-   
+      
    %Construct sub-matrices
    Lamg=Lam(inc_indices,inc_indices); 
    xxg=xx(inc_indices,inc_indices);
    xg=x(:,inc_indices);
    
    Bg=linsolve(xxg+Lamg,xg'*y);
-   logdensity(i)=0.5*log(det(Lamg))-0.5*log(det(Lamg+xxg))+0.5*phi*(Bg'*xg'*y)+sum(gamma_prop.*logpriorodds);
+   logdensity(i)=sum(gamma_prop)*log(phi)-0.5*phi*(y'*y-y'*xg*Bg)+sum(gamma_prop.*logpriorodds);
 end
 
 %Which Predictor gives greatest improvement
@@ -87,9 +92,11 @@ end
 %If improvement is positive, accept predictor and echo added predictor index
 if(sort_improv(1)>0) 
     gamma(sort_indices(1))=1;
+    Gam=diag(gamma);
+    B=Gam*linsolve(Gam*xx*Gam+Lam,Gam*x'*y);
     disp(sort_indices(1));
 end
-%%%%%%%%%%%%%%%%%%%%%%%%End Maximization of (Beta,Gamma)%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%End Maximization of (Beta,Gamma)%%%%%%%%%%%%%%%%%%%
 
 logdensityold=ones(p,1)*logdensity(sort_indices(1));
 end
